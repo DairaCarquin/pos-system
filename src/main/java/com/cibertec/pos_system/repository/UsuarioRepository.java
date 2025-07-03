de28d2c7573e63651b0836dee9fe2360e27aaa39
Margareth Vargas <margarethvargass@gmail.com>
2025-07-03 14:08:13 -0500

    se actualiza las funcionalidades

--- src/main/java/com/cibertec/pos_system/repository/UsuarioRepository.java
+++ src/main/java/com/cibertec/pos_system/repository/UsuarioRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
     // Esta es una consulta personalizada que busca un usuario por su nombre de usuario
-    @Query("SELECT u FROM UsuarioEntity u WHERE u.username = :username")
+    @Query("SELECT u FROM UsuarioEntity u WHERE u.username = :username and u.enabled = true")
     // Este método retorna un usuario que tenga ese username
     UsuarioEntity getUserByUsername(@Param("username") String username);
     //Se usa cuando queremos buscar un usuario (login)
